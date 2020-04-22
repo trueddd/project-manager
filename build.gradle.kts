5dd9d22cf9621ce60219046574300b27d2717964
trueddd <dmitry.kuzmich@orangesoft.co>
2020-04-23 01:00:02 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    application
+    kotlin("jvm") version "1.3.70"
+}
+
+group = "com.github.trueddd"
+version = "0.0.1"
+
+application {
+    mainClassName = "io.ktor.server.netty.EngineMain"
+}
+
+repositories {
+    mavenLocal()
+    jcenter()
+    maven { url = uri("https://kotlin.bintray.com/ktor") }
+}
+
+dependencies {
+    val ktorVersion = "1.3.2"
+    val kotlinVersion = "1.3.71"
+    val logbackVersion = "1.2.3"
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-auth:$ktorVersion")
+    implementation("io.ktor:ktor-auth-jwt:$ktorVersion")
+    implementation("io.ktor:ktor-gson:$ktorVersion")
+    testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
+}
+
+sourceSets {
+    kotlin.sourceSets["main"].kotlin.srcDirs("src")
+    kotlin.sourceSets["test"].kotlin.srcDirs("test")
+    kotlin.sourceSets["main"].kotlin.srcDirs("resources")
+}
+
+tasks.register("stage") {
+    dependsOn(tasks.named("clean"), tasks.named("build"))
+    mustRunAfter(tasks.named("clean"))
+}
+
+tasks.register<Jar>("fatJar") {
+    manifest {
+        attributes(
+                mapOf(
+                        "Main-Class" to application.mainClassName,
+                        "Class-Path" to configurations.compile
+                )
+        )
+    }
+    archiveBaseName.set("${project.name}-all")
+    from(Callable { configurations["runtimeClasspath"].map { if (it.isDirectory) it else zipTree(it) } })
+}
---
+++ src/Application.kt
@@ -0,0 +1,56 @@
+package com.example
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.features.*
+import org.slf4j.event.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.auth.*
+import io.ktor.gson.*
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, port = getPort(), module = Application::module).start(wait = true)
+}
+
+fun getPort() = System.getenv("PORT")?.toInt() ?: 8080
+
+fun Application.module() {
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        header("MyCustomHeader")
+        allowCredentials = true
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+
+    install(Authentication) {
+    }
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
